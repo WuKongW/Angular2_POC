d60e68901b18c80226aff2ca657192ac85929d2d
悟箜 <wukong@wukongdeMacBook-Pro.local>
2016-07-20 22:16:40 +0800

    inital commit

---
+++ app/common/components/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'wk-app',
+  template : '<h1>Hello Angular2 !</h1>'
+})
+export default class AppComponent {}
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import AppComponent from "./common/components/app.component";
+
+bootstrap(AppComponent, []).catch(err => console.error(err));
