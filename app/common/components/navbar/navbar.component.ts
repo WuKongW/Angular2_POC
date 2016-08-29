837b09fc9efb0a8bb6d0e41ba7492ca96799f701
WuKong <wanyongwang@hotmail.com>
2016-08-29 22:21:06 +0800

    Upgrade angular2 to rc5, router to rc1 (#3)
    * added Router component and configuration
    
    * upgrade Angular2 to RC5, Angular2-router to RC1
    
    * remove unused code

--- app/common/components/navbar/navbar.component.ts
+++ app/common/components/navbar/navbar.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {ROUTER_DIRECTIVES} from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     moduleId: 'app/common/components/navbar/',
     selector: 'wk-navbar',
     templateUrl: 'navbar.html',
-    styleUrls: ['navbar.css'],
-    directives: [ROUTER_DIRECTIVES]
+    styleUrls: ['navbar.css']
 })
 export class NavbarComponent {}
