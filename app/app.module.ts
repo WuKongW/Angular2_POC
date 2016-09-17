307f46ededbd1813471fb28144bf51c4b3a5642c
悟箜 <WuKongW@users.noreply.github.com>
2016-09-17 09:36:09 +0800

    upgrade to angular2 final

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule  } from '@angular/platform-browser';
 
 import { BlogModule } from './blog/blog.module';
 
+import {NavbarComponent} from './common/components/navbar/navbar.component';
+import HeaderComponent from './common/components/header/header.component';
+import FooterComponent from './common/components/footer/footer.component';
 import AppComponent from './common/components/app.component';
 import { HomeComponent } from './common/components/index';
 import { routing } from './common/router/app.routing';
@@ -13,7 +16,7 @@ import { routing } from './common/router/app.routing';
         BlogModule,
         routing,
     ],
-    declarations: [AppComponent, HomeComponent],
+    declarations: [AppComponent, HomeComponent, NavbarComponent,HeaderComponent, FooterComponent],
     bootstrap: [AppComponent],
 })
 
